e76f69f8a16c4d1062d161b2abde80e1636facdf
AleksanderMishchenko <34690816+AleksanderMishchenko@users.noreply.github.com>
2018-01-19 18:47:58 +0200

    class_work

---
+++ Student.java
@@ -0,0 +1,22 @@
+Pablic class Student{
+private int hours;
+private int works;
+
+
+public String hours()//геттер
+   return this.hours; 
+   }
+
+   public void hours(int hours) { //сеттер 
+      this.hours=hours;
+   }
+   
+   public String works()
+   return this.works; 
+   }
+
+   public void works(int works) {  
+      this.works=works;
+   }
+
+}
