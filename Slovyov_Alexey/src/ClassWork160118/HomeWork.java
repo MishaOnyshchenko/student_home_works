e2bc73f2f84cf8b81a86a4f0ade79761bf941b8f
Alexoriginal <34721585+Alexoriginal@users.noreply.github.com>
2018-01-19 18:53:53 +0200

    Create HomeWork.java

---
+++ Slovyov_Alexey/src/ClassWork160118/HomeWork.java
@@ -0,0 +1,34 @@
+public class HomeWork{
+    private  int numberOfWork;
+    private  int numberOfHours;
+    
+
+
+    public HomeWork(){
+    }
+    public HomeWor(int numberOfWork, int numberOfHours) {
+        this.numberOfWork = numberOfWork;
+        this.numberOfHourst = numberOfHours;
+        this.studMass = new Student[classLimit];
+    }
+    
+  
+
+   
+
+    public int getnumberOfWorkt() {
+        return numberOfWork;
+    }
+
+    public void setnumberOfWork(int numberOfWork) {
+        this.numberOfWork =numberOfWork;
+    }
+
+     public int getnumberOfHourst() {
+        return numberOfHourst;
+    }
+
+    public void setnumberOfHourst(int numberOfHourst) {
+        this.numberOfWork =numberOfWork;
+    }
+   
