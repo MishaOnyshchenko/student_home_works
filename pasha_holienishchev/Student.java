eda7e819fb58306dabb215a4be0de1dea957bff3
pashaShotgun <34775512+pashaShotgun@users.noreply.github.com>
2018-01-19 18:50:23 +0200

    Create Student.java

---
+++ pasha_holienishchev/Student.java
@@ -0,0 +1,12 @@
+public class Student
+{
+  private HomeWork HomeTask;
+  
+  public HomeWork getHomeTask(){
+    return HomeTask;
+  }
+  
+  public HomeWork getHomeTask(){
+    this.HomeTask = HomeTask;
+  }
+}
