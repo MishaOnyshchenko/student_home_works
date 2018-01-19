7aac389fc406f9ae553a7afffa2298d62a5e56bf
EgorPrihodko123 <34690750+EgorPrihodko123@users.noreply.github.com>
2018-01-19 18:47:12 +0200

    Create Homework.java
    Homework

---
+++ Homework.java
@@ -0,0 +1,14 @@
+int Num_Home;
+int Recourses;
+public int getNum_Home(){
+return.this Num_Home;
+}
+public void setNum_Home(int Num_Home){
+this.Num_Home=Num_Home;
+}
+public int getRecourses(){
+return.this Recourses;
+}
+public void setRecourses(int Recourses){
+this.Recourses=Recourses;
+}
