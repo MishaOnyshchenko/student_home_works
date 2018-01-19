0bafb51c2aff5327df372359e029a73f5c10dd02
pashaShotgun <34775512+pashaShotgun@users.noreply.github.com>
2018-01-19 18:46:26 +0200

    Create HomeWork.java

---
+++ pasha_holienishchev/HomeWork.java
@@ -0,0 +1,30 @@
+public class HomeWork
+{
+  private int hwNumber = 4;
+  private int hwHours = 15;
+  
+  public HomeWork(){
+  }
+  
+  public HomeWork(int hwNumber, int hwHours)
+  {
+    this.hwNumber = hwNumber;
+    this.hwHours = hwHours;
+  {
+  
+  public int gethwNumber() {
+        return hwNumber;
+    }
+  
+  public void sethwNumber(int hwNumber) {
+        this.hwNumber = hwNumber;
+    }
+  
+  public int gethwHours() {
+        return hwNumber;
+    }
+  
+  public void sethwHours(int hwHours) {
+        this.hwHours = hwHours;
+    }
+}
