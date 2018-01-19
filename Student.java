7253461bb4b7f143fbbbeb624fd21de82f56622c
AleksanderMishchenko <34690816+AleksanderMishchenko@users.noreply.github.com>
2018-01-19 18:52:30 +0200

    HomeWork_1

--- Student.java
+++ Student.java
@@ -1,22 +1,4 @@
 Pablic class Student{
-private int hours;
-private int works;
-
-
-public String hours()//геттер
-   return this.hours; 
-   }
-
-   public void hours(int hours) { //сеттер 
-      this.hours=hours;
-   }
-   
-   public String works()
-   return this.works; 
-   }
-
-   public void works(int works) {  
-      this.works=works;
-   }
+HomeWork home = HomeWork();
 
 }
